865b8e7c3801449f81e09c7888bb33cb7d037d74
Melva Matthews <mmatt26@lsu.edu>
2017-02-16 12:02:05 -0600

    Cretes circles with different transparent colors

---
+++ Prog2/Sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(200, 200);
+}
+
+function draw() {
+  background(255);
+   noStroke();
+   fill(0, 0, 255, 90);
+   ellipse(70, 130 , 110, 110);
+   fill(0, 255, 0, 90);
+   ellipse(140, 130 , 110, 110);
+   fill(255, 0, 0, 90);
+   ellipse(105, 70 , 110, 110);
+}
